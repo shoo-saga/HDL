e6e8cc15151e3d69dfa7ed2b0742136a21abb3cc
shoo-saga <7a.show@gmail.com>
2020-07-15 21:08:24 +0900

    create mkwe and arb module

--- last/arb.v
+++ last/arb.v
@@ -1,4 +1,15 @@
+`include "sw.vh"
 module arb(input ack0,ack1,ack2,ack3, output req0,req1,req2,req3);
 
-
+always_comb begin
+    if (ack0) req0 = `ASSERT;
+    else if (ack1) req1 = `ASSERT;
+    else if (ack2) req2 = `ASSERT;
+    else if (ack3) req3 = `ASSERT;
+    else
+    req0 = `NEGATE;
+    req1 = `NEGATE;
+    req2 = `NEGATE;
+    req3 = `NEGATE;
+end
 endmodule
--- last/mkwe.v
+++ last/mkwe.v
@@ -1,3 +1,11 @@
-module mkwe();
-
+`include "sw.vh"
+module mkwe(input [`PKTW:0] pkti, output we);
+    always_comb begin
+        if (pkti [9:8] == HEAD)
+        we = `ASSERT;
+        else if (pkti [9:8] == HEAD)
+        we = `ASSERT;
+        else
+        we = `NEGATE;
+    end
 endmodule
